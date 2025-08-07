637c9a44c05ef4a154b2b4bebf3fecd5adc54ae2
Ben Mayer <info@leocodes.net>
2025-08-07 21:10:45 +0200

    bevy: reworked template to fit new rust template

---
+++ templates/bevy/flake.nix
@@ -0,0 +1,121 @@
+{
+  description = "DESC";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-utils.url = "github:numtide/flake-utils";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+
+    crane.url = "github:ipetkov/crane";
+    advisory-db = {
+      url = "github:rustsec/advisory-db";
+      flake = false;
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      systems,
+      flake-utils,
+      treefmt-nix,
+      crane,
+      advisory-db,
+      rust-overlay,
+    }:
+    flake-utils.lib.eachSystem (import systems) (
+      system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        inherit (pkgs) lib;
+        craneLib = (crane.mkLib pkgs).overrideToolchain (
+          p: p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
+        );
+
+        buildInputs = with pkgs; [
+          udev
+          alsa-lib-with-plugins
+          vulkan-loader
+          xorg.libX11
+          xorg.libXcursor
+          xorg.libXi
+          xorg.libXrandr
+          libxkbcommon
+          wayland
+        ];
+        nativeBuildInputs = with pkgs; [
+          mold-wrapped
+          clang
+          pkg-config
+        ];
+
+        src = craneLib.cleanCargoSource ./.;
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+
+          inherit buildInputs nativeBuildInputs;
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        APP = craneLib.buildPackage (
+          commonArgs
+          // {
+            # dynamic linking not working with nix
+            cargoExtraArgs = "--locked --no-default-features";
+
+            inherit cargoArtifacts;
+            meta.description = "DESC";
+          }
+        );
+
+        treefmtEval = treefmt-nix.lib.evalModule pkgs ./treefmt.nix;
+      in
+      {
+        formatter = treefmtEval.config.build.wrapper;
+
+        checks = {
+          inherit APP;
+          formatting = treefmtEval.config.build.check self;
+          clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            }
+          );
+          audit = craneLib.cargoAudit { inherit src advisory-db; };
+          nextest = craneLib.cargoNextest (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              partitions = 1;
+              partitionType = "count";
+              cargoNextestPartitionsExtraArgs = "--no-tests=pass";
+            }
+          );
+        };
+
+        packages = {
+          inherit APP;
+          default = APP;
+        };
+
+        devShells.default = craneLib.devShell {
+          packages = buildInputs ++ nativeBuildInputs;
+          LD_LIBRARY_PATH = lib.makeLibraryPath buildInputs;
+        };
+      }
+    );
+}
